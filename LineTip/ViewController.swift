879b3e32c241f36a8840b8e52db0c991e59d12c9
Artur Schäfer <schaefa@Karin-Monsers-Mac-Pro.local>
2015-11-27 10:14:11 +0100

    initial commit

---
+++ LineTip/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LineTip
+//
+//  Created by Artur Schäfer on 27.11.15.
+//  Copyright © 2015 Artur Schäfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onLineDetectionBtn(sender: AnyObject) {
+        print("line-detection btn clicked! :)");
+    }
+
+}
+
---
+++ LineTipTests/LineTipTests.swift
@@ -0,0 +1,36 @@
+//
+//  LineTipTests.swift
+//  LineTipTests
+//
+//  Created by Artur Schäfer on 27.11.15.
+//  Copyright © 2015 Artur Schäfer. All rights reserved.
+//
+
+import XCTest
+@testable import LineTip
+
+class LineTipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

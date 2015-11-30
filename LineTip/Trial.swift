99e46c61ae52031ed22494a68716bb5db5205463
lidox <schaefa@mail.ru>
2015-11-30 16:58:47 +0100

    - add model classes and user manager view in beta

---
+++ LineTip/Trial.swift
@@ -0,0 +1,36 @@
+//
+//  Trial.swift
+//  LineTip
+//
+//  Created by Artur Schäfer on 30.11.15.
+//  Copyright © 2015 Artur Schäfer. All rights reserved.
+//
+
+import UIKit
+
+class Trial: NSObject {
+    var hits : Int = 0
+    var fails : Int = 0
+    var timeStamp: String
+    var duration: String = ""
+    
+    override init(){
+        self.timeStamp = "\(NSDate().timeIntervalSince1970 * 1000)"
+    }
+    
+    func getTimeBySec(totalSeconds: Int) -> String {
+        let seconds = totalSeconds % 60
+        let minutes = (totalSeconds / 60) % 60
+        let hours = totalSeconds / 3600
+        let strHours = hours > 9 ? String(hours) : "0" + String(hours)
+        let strMinutes = minutes > 9 ? String(minutes) : "0" + String(minutes)
+        let strSeconds = seconds > 9 ? String(seconds) : "0" + String(seconds)
+        
+        if hours > 0 {
+            return "\(strHours):\(strMinutes):\(strSeconds)"
+        }
+        else {
+            return "\(strMinutes):\(strSeconds)"
+        }
+    }
+}
---
+++ User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  LineTip
+//
+//  Created by Artur Schäfer on 30.11.15.
+//  Copyright © 2015 Artur Schäfer. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    let medId : String
+    
+    init(medId : String){
+        self.medId = medId
+    }
+}

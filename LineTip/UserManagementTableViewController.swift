2d0fa8d5b728ee7b3dc76cce660c07e7934b69fe
lidox <schaefa@mail.ru>
2015-12-03 10:28:04 +0100

    add half version

--- LineTip/UserManagementTableViewController.swift
+++ LineTip/UserManagementTableViewController.swift
@@ -8,21 +8,43 @@
 
 import UIKit
 
-class UserManagementTableViewController: UIViewController {
+class UserManagementTableViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var names = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad()")
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
     }
 
+    @IBAction func addName(sender: AnyObject) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return names.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+            
+            cell!.textLabel!.text = names[indexPath.row]
+            
+            return cell!
     }
 
 }

7ff7685f9da6f11138a80b3d7460d4fa5d08e46f
lidox <schaefa@mail.ru>
2016-02-19 15:28:43 +0100

    add refactor utils code

--- LineTip/Utils.swift
+++ LineTip/Utils.swift
@@ -49,4 +49,47 @@ class Utils {
         
         return 0
     }
+    
+    
+    class func loadSettingsData(){
+        
+        var settingsList = [String]()
+        settingsList.append(ConfigKey.LINE_WIDTH)
+        settingsList.append(ConfigKey.LINE_COLOR_ALPHA)
+        settingsList.append(ConfigKey.SPOT_HEIGHT)
+        settingsList.append(ConfigKey.SPOT_WIDTH)
+        settingsList.append(ConfigKey.SPOT_IMAGE_NAME)
+        
+        for (index, item) in settingsList.enumerate() {
+            //print("Item \(index): \(item)")
+            let test = Utils.getSettingsData(item)
+            
+            if(test  as! NSObject == 0){
+                print("could not load settings data for key: \(item) . So use defaults")
+                
+                if(item == ConfigKey.LINE_WIDTH){
+                    Utils.setSettingsData(ConfigKey.LINE_WIDTH, value: 5.0)
+                }
+                else if(item == ConfigKey.LINE_COLOR_ALPHA){
+                    Utils.setSettingsData(ConfigKey.LINE_COLOR_RED, value: 255)
+                    Utils.setSettingsData(ConfigKey.LINE_COLOR_GREEN, value: 255)
+                    Utils.setSettingsData(ConfigKey.LINE_COLOR_BLUE, value: 255)
+                    Utils.setSettingsData(ConfigKey.LINE_COLOR_ALPHA, value: 255)
+                }
+                else if(item == ConfigKey.SPOT_HEIGHT){
+                    Utils.setSettingsData(ConfigKey.SPOT_HEIGHT, value: 75)
+                }
+                else if(item == ConfigKey.SPOT_IMAGE_NAME){
+                    Utils.setSettingsData(ConfigKey.SPOT_IMAGE_NAME, value: "trans.png")
+                }
+                else if(item == ConfigKey.SPOT_WIDTH){
+                    Utils.setSettingsData(ConfigKey.SPOT_WIDTH, value: 75)
+                }
+            }
+            else{
+                print("loaded settings data for key: \(item) at pos: \(index)")
+            }
+        }
+        
+    }
 }

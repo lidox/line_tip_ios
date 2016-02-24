174472f63cb32d60966a2624245d781c98b8cd44
lidox <schaefa@mail.ru>
2016-02-24 09:40:35 +0100

    add generic chart to statistics

--- LineTip/StatisticsViewController.swift
+++ LineTip/StatisticsViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import QuartzCore
 
-class StatisticsViewController: UIViewController {
+class StatisticsViewController: UIViewController, LineChartDelegate {
+    
+    var label = UILabel()
+    var lineChart: LineChart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var views: [String: AnyObject] = [:]
+        
+        label.text = "..."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-80-[label]", options: [], metrics: nil, views: views))
+        
+        // simple arrays
+        let data: [CGFloat] = [3, 4, -2, 11, 13, 15]
+        let data2: [CGFloat] = [1, 3, 5, 13, 17, 20]
+        
+        // simple line with custom x axis labels
+        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        
+        lineChart = LineChart()
+        lineChart.animation.enabled = true
+        lineChart.area = true
+        lineChart.x.labels.visible = true
+        lineChart.x.grid.count = 5
+        lineChart.y.grid.count = 5
+        lineChart.x.labels.values = xLabels
+        lineChart.y.labels.visible = true
+        lineChart.addLine(data)
+        lineChart.addLine(data2)
+        
+        lineChart.translatesAutoresizingMaskIntoConstraints = false
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: [], metrics: nil, views: views))
         // Do any additional setup after loading the view.
     }
     
@@ -20,16 +58,22 @@ class StatisticsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBOutlet weak var changeColorButton: UIButton!
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
 
+    /**
+     * Line chart delegate method.
+     */
+    func didSelectDataPoint(x: CGFloat, yValues: Array<CGFloat>) {
+        label.text = "x: \(x)     y: \(yValues)"
+    }
+    
+    
+    
+    /**
+     * Redraw chart on device rotation.
+     */
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        if let chart = lineChart {
+            chart.setNeedsDisplay()
+        }
+    }
 }

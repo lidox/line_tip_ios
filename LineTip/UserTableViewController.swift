35049501685eefe6f04efa7eb1eeaeb66bb4cd6f
lidox <schaefa@mail.ru>
2016-03-07 17:56:57 +0100

    add text to wellcome image

--- LineTip/UserTableViewController.swift
+++ LineTip/UserTableViewController.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class UserTableViewController: UITableViewController {
 
-    
+    @IBOutlet weak var wellcomeImageView: UIImageView!
     @IBOutlet weak var addButton: UIBarButtonItem!
     @IBOutlet weak var navigation: UINavigationItem!
     
@@ -21,6 +21,7 @@ class UserTableViewController: UITableViewController {
     
     @IBOutlet weak var headerView: UIView!
     
+    var image : UIImage!
     var medUserList = [MedUser]()
     
     override func viewDidLoad() {
@@ -33,8 +34,10 @@ class UserTableViewController: UITableViewController {
         Utils.loadSettingsData()
         
         initTitleAndColors()
+        
         initWelcomeImage()
         
+        wellcomeImageView.setNeedsDisplay()
         medUserList = MedUserManager.fetchMedUsers()
     }
 
@@ -167,13 +170,16 @@ class UserTableViewController: UITableViewController {
         self.navigationController?.navigationBar.titleTextAttributes = titleDict as? [String : AnyObject]
         //self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
         self.navigationController?.navigationBar.tintColor = UIColor.myKeyColor()
+        
     }
     
     func initWelcomeImage() {
-        let imageName = "wellcome_orange.jpg"
-        let image = UIImage(named: imageName)
+        let imageName = "splash-overlay.png"
+        image = UIImage(named: imageName)
+        wellcomeImageView.backgroundColor = UIColor.myKeyColor()
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         wellcomeImage.frame = CGRectMake(0,0, screenSize.width, screenSize.height * 0.33)
+        image = textToImage("BOOM BABY", inImage: image, atPoint: CGPointMake(100, 100))
         wellcomeImage.image = image
     }
     
@@ -186,6 +192,41 @@ class UserTableViewController: UITableViewController {
         else {
             return true;
         }
+    }
+    
+    func textToImage(drawText: NSString, inImage: UIImage, atPoint:CGPoint)->UIImage{
+        
+        // Setup the font specific variables
+        let textColor: UIColor = UIColor.blackColor()
+        let textFont: UIFont = UIFont(name: "Helvetica Bold", size: 25)!
+        
+        //Setup the image context using the passed image.
+        UIGraphicsBeginImageContext(inImage.size)
+        
+        //Setups up the font attributes that will be later used to dictate how the text should be drawn
+        let textFontAttributes = [
+            NSFontAttributeName: textFont,
+            NSForegroundColorAttributeName: textColor,
+        ]
+        
+        //Put the image into a rectangle as large as the original image.
+        inImage.drawInRect(CGRectMake(0, 0, inImage.size.width, inImage.size.height))
+        
+        // Creating a point within the space that is as bit as the image.
+        let rect: CGRect = CGRectMake(atPoint.x, atPoint.y, inImage.size.width, inImage.size.height)
+        
+        //Now Draw the text into an image.
+        drawText.drawInRect(rect, withAttributes: textFontAttributes)
+        
+        // Create a new image out of the images we have created
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // End the context now that we have the image we need
+        UIGraphicsEndImageContext()
+        
+        //And pass it back up to the caller.
+        return newImage
+        
     }
 }
 
@@ -202,13 +243,11 @@ extension UINavigationController {
 }
 
 extension UIAlertController {
-    /*public override func supportedInterfaceOrientations() -> Int {
-        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
-    }*/
     
     override public func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return [.Portrait, .PortraitUpsideDown]
     }
+    
     public override func shouldAutorotate() -> Bool {
         return false
     }

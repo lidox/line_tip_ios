b6f0b19a057e19163c2de70a22d1538b3d0d7e9e
lidox <schaefa@mail.ru>
2016-03-10 13:39:57 +0100

    connect quickstart ui elements

--- LineTip/QuickStartUIViewController.swift
+++ LineTip/QuickStartUIViewController.swift
@@ -11,4 +11,21 @@ import UIKit
 
 class QuickStartUIViewController: UIViewController {
 
+    
+    @IBOutlet weak var assignUserLabel: UILabel!
+    @IBOutlet weak var assignUserSubtitleLabel: UILabel!
+    @IBOutlet weak var noUserTitleLabel: UILabel!
+    @IBOutlet weak var noUserSubTitleLabel: UILabel!
+    
+    @IBOutlet weak var createUserButton: UIButton!
+    @IBOutlet weak var startTrialButton: UIButton!
+    
+    
+    @IBAction func onCreateUserButtonClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onStartTrialButtonClick(sender: AnyObject){
+        
+    }
 }

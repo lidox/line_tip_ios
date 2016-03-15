09cebb11a7c9b4f05374aba7d1b9a888ddae254d
lidox <schaefa@mail.ru>
2016-03-15 22:52:03 +0100

    add app settings

---
+++ LineTip/AppSettingsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AppSettingsViewController.swift
+//  LineTip
+//
+//  Created by Artur Schäfer on 15.03.16.
+//  Copyright © 2016 Artur Schäfer. All rights reserved.
+//
+
+import UIKit
+
+class AppSettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //http://www.edumobile.org/ios/creating-grouped-table-views-in-swift/
+    var array = [ ["Moscow", "Saint Petersburg", "Novosibirsk", "Novosibirsk", "Nizhny Novgorod", "Samara", "Omsk" ],
+        
+        ["Kiyv", "Odessa", "Donetsk", "Harkiv", "Lviv", "Uzhgorod", "Zhytomyr", "Luhansk", "Mikolayv", "Kherson"],
+        
+        ["Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt", "Stuttgart", "Düsseldorf", "Dortmund", "Essen", "Bremen"]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return array.count
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return array[section].count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell;
+        
+        cell.textLabel?.text = array[indexPath.section][indexPath.row]
+        
+        return cell
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
